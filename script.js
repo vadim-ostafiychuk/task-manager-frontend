5e7796dce6273e4f4f53843035e58d41672a4f20
Vadim Ostafiychuk <vadim.ostafiychuk@outlook.com>
2026-05-06 14:27:50 +0300

    searcInput eventListeners

--- script.js
+++ script.js
@@ -1,72 +1,56 @@
+// STATE
 let editingIndex = null;
 let filter = "all";
-
-function setFilter(value) {
-  filter = value;
-
-  renderTasks();
-}
-
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+let searchQuery = "";
 
+document.getElementById("taskList").addEventListener("click", (e) => {
+  const target = e.target.closest("[data-action]");
+  if (!target) return;
+
+  const action = e.target.dataset.action;
+  const index = e.target.dataset.index;
+
+  if (!action) return;
+
+  switch (action) {
+    case "save": {
+      saveEdit(+index);
+
+      break;
+    }
+    case "cancel": {
+      cancelEdit();
+
+      break;
+    }
+    case "edit": {
+      startEdit(+index);
+
+      break;
+    }
+    case "delete": {
+      removeTask(+index);
+
+      break;
+    }
+    case "toggle": {
+      toggleTask(index);
+      break;
+    }
+  }
+});
+
+const handleSearch = debounce((e) => {
+  searchQuery = e.target.value.toLowerCase();
+  renderTasks();
+}, 300);
+
+document.getElementById("searchInput").addEventListener("input", handleSearch);
+
+//Actions
 function saveTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function renderTasks() {
-  const list = document.getElementById("taskList");
-  list.innerHTML = "";
-
-  if (!tasks.length) {
-    list.innerHTML = "<span>No Tasks... Please add your first Task</span>";
-  }
-
-  let filteredTasks = tasks;
-
-  if (filter === "active") {
-    filteredTasks = tasks.filter((t) => !t.done);
-  }
-
-  if (filter === "completed") {
-    filteredTasks = tasks.filter((t) => t.done);
-  }
-
-  filteredTasks.forEach((taskObj) => {
-    const index = tasks.indexOf(taskObj);
-
-    const isEditing = editingIndex === index;
-
-    const task = document.createElement("div");
-    task.className = "task";
-
-    task.innerHTML = `
-  <input type="checkbox" ${taskObj.done ? "checked" : ""} onchange="toggleTask(${index})" />
-
-  ${
-    isEditing
-      ? `<input id="editInput-${index}" value="${taskObj.text}" />`
-      : `<span style="${taskObj.done ? "text-decoration: line-through; opacity:0.6;" : ""}">
-          ${taskObj.text}
-        </span>`
-  }
-
-  <div>
-    ${
-      isEditing
-        ? `
-          <button onclick="saveEdit(${index})">Save</button>
-          <button onclick="cancelEdit()">Cancel</button>
-        `
-        : `
-          <button onclick="startEdit(${index})">Edit</button>
-          <button onclick="removeTask(${index})">X</button>
-        `
-    }
-  </div>
-`;
-
-    list.appendChild(task);
-  });
 }
 
 function addTask() {
@@ -80,16 +64,6 @@ function addTask() {
   renderTasks();
 
   input.value = "";
-}
-
-function startEdit(index) {
-  editingIndex = index;
-  renderTasks();
-}
-
-function cancelEdit() {
-  editingIndex = null;
-  renderTasks();
 }
 
 function saveEdit(index) {
@@ -117,4 +91,110 @@ function toggleTask(index) {
   renderTasks();
 }
 
+// UI Actions
+
+function setFilter(value) {
+  filter = value;
+
+  renderTasks();
+}
+
+function startEdit(index) {
+  editingIndex = index;
+  renderTasks();
+}
+
+function cancelEdit() {
+  editingIndex = null;
+  renderTasks();
+}
+
+// UI
+function renderTasks() {
+  const list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  if (!tasks.length && filter === "all") {
+    list.innerHTML = "<span>No Tasks... Please add your first Task</span>";
+    return;
+  }
+
+  let filteredTasks = tasks;
+
+  if (filter === "active") {
+    filteredTasks = tasks.filter((t) => !t.done);
+  }
+
+  if (filter === "completed") {
+    filteredTasks = tasks.filter((t) => t.done);
+  }
+
+  if (!filteredTasks.length) {
+    list.innerHTML = "<span>No tasks for this filter</span>";
+    return;
+  }
+
+  if (searchQuery) {
+    filteredTasks = filteredTasks.filter((t) =>
+      t.text.toLowerCase().includes(searchQuery),
+    );
+
+    if (!filteredTasks.length) {
+      list.innerHTML = "<span>No tasks for this search</span>";
+      return;
+    }
+  }
+
+  filteredTasks.forEach((taskObj) => {
+    const index = tasks.indexOf(taskObj);
+
+    const isEditing = editingIndex === index;
+
+    const task = document.createElement("div");
+    task.className = "task";
+
+    task.innerHTML = `
+  <input type="checkbox" ${taskObj.done ? "checked" : ""} data-action="toggle" data-index="${index}" />
+
+  ${
+    isEditing
+      ? `<input id="editInput-${index}" value="${taskObj.text}" />`
+      : `<span style="${taskObj.done ? "text-decoration: line-through; opacity:0.6;" : ""}">
+          ${taskObj.text}
+        </span>`
+  }
+
+  <div>
+    ${
+      isEditing
+        ? `
+          <button data-action="save" data-index="${index}">Save</button>
+          <button data-action="cancel" data-index="${index}">Cancel</button>
+        `
+        : `
+          <button data-action="edit" data-index="${index}">Edit</button>
+          <button data-action="delete" data-index="${index}">X</button>
+        `
+    }
+  </div>
+`;
+
+    list.appendChild(task);
+  });
+}
+
 renderTasks();
+
+
+//UTILLS
+function debounce(fn, delay) {
+  let timeout;
+
+  return function (...args) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
